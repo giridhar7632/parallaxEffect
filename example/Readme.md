449ad48b7bb3ceb6d1f6e61df414e821fa1cbecf
giridhar7632 <giridhar.talla2002@gmail.com>
2020-09-15 21:36:21 +0530

    updated parallax

--- example/Readme.md
+++ example/Readme.md
@@ -1,5 +1,3 @@
 # Example on parallax effect
 
 live at (https://parallaxeffect.giridharhackclu.repl.co/)
-
-<img src="https://cloud-im9mhrbdn.vercel.app/annotation_2020-09-15_181842.png">
--- example/app.js
+++ example/app.js
@@ -1,10 +1,10 @@
 const parallax = (element, distance, speed) => {
   const item = document.querySelector(element);
 
-  item.style.transform = `translateY(-${distance*speed}px)`;
+  item.style.transform = `translateY(${distance*speed}px)`;
 }
 
 window.addEventListener('scroll', () => {
-  parallax('.faster', window.scrollY, 1.5);
+  parallax('.faster', -window.scrollY, 0.5);
   parallax('.slower', window.scrollY, 0.5);
 });
