8ced6fb8c3df46020da67656dacb74694f51f912
giridhar7632 <giridhar.talla2002@gmail.com>
2020-09-18 16:41:38 +0530

    uodating repo

--- project/script.js
+++
@@ -1,9 +0,0 @@
-const parallax = (element, distance, speed) => {
-  const item = document.querySelector(element);
-  item.style.transform = `translateY(${distance*speed}px)`;
-}
-window.addEventListener('scroll', () => {
-  //console.log(window.scrollY)
-  parallax('.faster', -window.scrollY, 0.5);
-  parallax('.slower', window.scrollY, 0.37);
-});
