06468ad8cb9dbc8c580f6f868e1327efd3074242
giridhar7632 <giridhar.talla2002@gmail.com>
2020-09-20 18:15:08 +0530

    Update Example

--- example/app.js
+++ example/app.js
@@ -1,10 +1,12 @@
 const parallax = (element, distance, speed) => {
   const item = document.querySelector(element);
 
-  item.style.transform = `translateY(${distance*speed}px)`;
+  item.style.transform = `translateY(${-distance*speed}px)`;
 }
-
+ 
 document.addEventListener('scroll', () => {
-  parallax('.faster', -window.scrollY, 0.5);
-  parallax('.slower', window.scrollY, 0.4);
+  parallax('.layer-1', window.scrollY, 0.5);
+  parallax('.layer-2', window.scrollY, -0.4);
+  parallax('.layer-4', -window.scrollY, 0.7);
+  parallax('.layer-5', -window.scrollY, 0.6);
 });
