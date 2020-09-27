175f599fc5de0b9d03bffec9eb3683b841485f55
giridhar7632 <giridhar.talla2002@gmail.com>
2020-09-27 13:27:49 +0530

    added parallax

--- example_three/script.js
+++ example_three/script.js
@@ -1,11 +1,9 @@
 const parallax = (layer, distance, speed) => {
-  layer.style.transform = `translate(${distance*speed}px, ${distance*1}px);`
+  item = document.querySelector(layer)
+  item.style.transform = `translate(${distance*speed}px, ${distance*1}px);`
 }
 document.addEventListener('scroll', () => {
-  document.querySelectorAll('.up').forEach(up => {
-    parallax(up, window.scrollY, -0.7)
-  })
-  document.querySelectorAll('.down').forEach(down => {
-    parallax(down, window.scrollY, 0.5)
-  })
+  
+    parallax('#up', window.scrollY, -0.7)
+    parallax('.down', window.scrollY, 0.5)
 })
