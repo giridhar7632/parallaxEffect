034c0a7e1f0a426d41d2b7685876e3bb0f69ece0
giridhar7632 <giridhar.talla2002@gmail.com>
2020-09-29 18:49:07 +0530

    added more projects

--- example_three/script.js
+++
@@ -1,9 +0,0 @@
-const parallax = (layer, distance, speed) => {
-  item = document.querySelector(layer)
-  item.style.transform = `translate(${distance*speed}px, ${distance*1}px);`
-}
-document.addEventListener('scroll', () => {
-  
-    parallax('#up', window.scrollY, -0.7)
-    parallax('.down', window.scrollY, 0.5)
-})
--- horizontalParallax/script.js
+++ horizontalParallax/script.js
@@ -1,19 +1,3 @@
-// Horizontal Parallax
-const hrParallax = (element, distance, xSpeed, ySpeed=0) => {
-  const item = document.querySelector(element)
-  
-  item.style.transform = `translate(${distance*xSpeed}px,${distance*ySpeed}px)`
-}
-
-document.addEventListener('scroll', () => {
-  hrParallax('.cl1', window.scrollY, 0.4, 0.5)
-  hrParallax('.cl2', window.scrollY, 0.7, 0.6)
-  hrParallax('.cl3', window.scrollY, 0.3, 0.3)
-  hrParallax('.cl4', window.scrollY, 0.6, 0.5)
-  hrParallax('.cl5', window.scrollY, 0.4, 0.3)
-  hrParallax('.pl', window.scrollY, 2, -0.5)
-})
-
 // Parallax on mouse move
 const mmParallax = (element, xDistance, yDistance, speed) => {
   let x = (window.innerWidth - xDistance*speed)/100
